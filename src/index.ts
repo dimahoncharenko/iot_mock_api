1c8b55441e15831b918b4c3ca5a04ef08507122c
Dima Honcharenko <d.goncharenko.man@gmail.com>
2023-08-09 18:16:38 +0300

    Added a domain to cors policy

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
     cors: {
-        origin: "http://localhost:4200",
+        origin: ["http://localhost:4200", "https://iot-widgets.web.app/"],
         methods: ["GET", "POST"]
     }
 });
